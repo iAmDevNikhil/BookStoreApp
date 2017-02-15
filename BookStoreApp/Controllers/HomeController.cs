dc7151a5694aacbf7aeabe0ed5b56dc79ac7bbf6
nikhildevaraj <nikhildevaraj1991@gmail.com>
2017-02-15 13:40:01 -0500

    Created Database, Models, Controller and Views

--- BookStoreApp/Controllers/HomeController.cs
+++ BookStoreApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookStoreApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,99 @@ namespace BookStoreApp.Controllers
 {
     public class HomeController : Controller
     {
+        private BooksDBEntities _db = new BooksDBEntities(); 
+        // GET: Home
         public ActionResult Index()
         {
+            return View(_db.Books.ToList());
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
             return View();
         }
 
-        public ActionResult About()
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude = "Id")] Book bookToCreate)
         {
-            ViewBag.Message = "Your application description page.";
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View();
+                _db.Books.Add(bookToCreate);
+                _db.SaveChanges();
 
-            return View();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
-        public ActionResult Contact()
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
         {
-            ViewBag.Message = "Your contact page.";
+            var bookToEdit = (from b in _db.Books
+                              where b.Id == id
+                              select b).First();
 
-            return View();
+            return View(bookToEdit);
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Book bookToEdit)
+        {
+            try
+            {
+                var originalBook = (from b in _db.Books
+                                    where b.Id == bookToEdit.Id
+                                    select b).First();
+                if (!ModelState.IsValid)
+                    return View(originalBook);
+                _db.ApplyPropertyChanges(originalBook, bookToEdit);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var bookToDelete = (from b in _db.Books
+                              where b.Id == id
+                              select b).First();
+
+            return View(bookToDelete);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Book bookToDelete)
+        {
+            try
+            {
+                var originalBook = (from b in _db.Books
+                                    where b.Id == bookToDelete.Id
+                                    select b).First();
+                if (!ModelState.IsValid)
+                    return View(originalBook);
+                _db.ApplyPropertyChanges(originalBook);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
